4afdfcb4ac220a693be17224cfd88f84f525cdf5
winter_x64 <75623356+winter-x64@users.noreply.github.com>
2023-11-09 14:56:39 +0530

    ✌️ Add project config file

--- src/views/home.py
+++ src/views/home.py
@@ -1,8 +1,16 @@
 from flask import Blueprint, render_template
 
+from ..models import events
+
 home = Blueprint("home", __name__)
 
 
 @home.route("/", methods=["GET", "POST"])
 def index():
     return render_template("home/home.html")
+
+
+@home.route("/lister", methods=["GET", "POST"])
+def events():
+    event = events.query.all()
+    return render_template("home/home.html", event=event)
